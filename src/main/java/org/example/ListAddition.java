3f0f031c85cf375b3799716c87de7b2c8f75ec47
Rafal <rafalwieczorek881@gmail.com>
2023-07-12 23:24:25 +0200

    added task description

--- src/main/java/org/example/ListAddition.java
+++ src/main/java/org/example/ListAddition.java
@@ -7,6 +7,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ListAddition {
+
+    /*
+     This problem was recently asked by Google.
+
+     Given a list of numbers and a number k, return whether any two numbers
+     from the list add up to k.
+
+     For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+     */
+
     public static void main(String[] args) {
 
         int k = 17;
